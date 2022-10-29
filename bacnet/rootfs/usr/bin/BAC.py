26128814883820a73adfa3fa4d60ba5aedd318d2
luuk- <luuk-@DESKTOP-RQDB4D0>
2022-10-29 21:50:08 +0200

    w

--- bacnet/rootfs/usr/bin/BAC.py
+++ bacnet/rootfs/usr/bin/BAC.py
@@ -192,7 +192,7 @@ def main():
 
     while True:
         run()
-        app.run(host = '127.0.0.1',port=5000, debug= True)
+        app.run(host = '0.0.0.0',port=5000, debug= True)
 
     _log.debug("fini")
 
