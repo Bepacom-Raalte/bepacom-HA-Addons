d9b659729c27dd99a699fa001e0b0fb06e13cc58
Luuk <lhwgeertsema@outlook.com>
2022-11-17 08:21:02 +0100

    Trying more discovery :DD

--- bacnet/rootfs/usr/bin/ding.py
+++ bacnet/rootfs/usr/bin/ding.py
@@ -2,51 +2,55 @@ devicelist = [("device",0),("device",1),("device",2)]
 objectlist = [("analogValue",0), ("analogValue",1),("binaryValue",0)]
 devices = dict()
 
+# Objects die ik wil ondersteunen
+# AnalogInput, AnalogOutput, AnalogValue, BinaryInput, BinaryOutput, BinaryValue, 
+# MultiStateInput, MultiStateOutput, MultiStateValue, Device, LargeAnalogValue,
+# IntegerValue, PositiveIntegerValue
+
 BACnet_object = {
-    "object_identifier": None,
-    "object_name": None,
-    "object_type": None,
-    "present_value": None,
+    "objectIdentifier": None,
+    "objectName": None,
+    "objectType": None,
+    "presentValue": None,
     "description": None,
     "reliability": None,
-    "out_of_service": None,
+    "outOfService": None,
     "units": None,
-    "notification_class": None 
+    "notificationClass": None 
     }
 
 device = {
-    "object_identifier": None,
-    "object_name": None,
-    "object_type": None,
-    "system_status": None,
-    "vendor_name": None,
-    "vendor_identifier": None,
-    "model_name": None,
-    "firmware revision": None,
-    "application_software_version": None,
-    "protocol_version": None,
-    "protocol_revision": None,
-    "protocol_services_supported": None,
-    "protocol_object_types_supported": None,
-    "objectlist": dict(),
-    "max_apdu_length_accepted": None,
-    "segmentation_supported": None,
-    "apdu_segment_timeout": None,
-    "apdu_timeout": None,
-    "number_of_apdu_retries": None,
-    "device_address_binding": None,
-    "database_revision": None
+    "objectIdentifier": None,
+    "objectName": None,
+    "objectType": None,
+    "systemStatus": None,
+    "vendorName": None,
+    "vendorIdentifier": None,
+    "modelName": None,
+    "firmwareRevision": None,
+    "applicationSoftwareVersion": None,
+    "protocolVersion": None,
+    "protocolRevision": None,
+    "protocolServicesSupported": None,
+    "protocolObjectTypesSupported": None,
+    "objectList": dict(),
+    "maxApduLengthAccepted": None,
+    "segmentationSupported": None,
+    "apduSegmentTimeout": None,
+    "apduTimeout": None,
+    "numberOfApduRetries": None,
+    "deviceAddressBinding": None,
+    "databaseRevision": None
     }
 
 
 for entry in devicelist:
     devices.update({str(entry[0])+":"+str(entry[1]): device})
 
-#print(devices)
 
 for x,y in devices.items():
     for object in objectlist:
-        y["objectlist"].update({str(object[0])+ ":" +str(object[1]): BACnet_object})
+        y["objectList"].update({str(object[0])+ ":" +str(object[1]): BACnet_object})
     print(x,y)
 
 
