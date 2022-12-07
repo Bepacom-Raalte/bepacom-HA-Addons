3a097a6f0a8fb4ea6153b32a67113041aaafb1d4
Luuk <lhwgeertsema@outlook.com>
2022-12-07 09:01:06 +0100

    Websocket updated to concurrently read and write instead of blocking each other

--- bacnet/rootfs/usr/bin/webAPI.py
+++ bacnet/rootfs/usr/bin/webAPI.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.wsgi import WSGIMiddleware
 from flask import Flask, jsonify, escape, request, render_template
+import asyncio
 import json
 import sys
 
@@ -63,31 +64,56 @@ async def root():
 async def nicepage():
     return {"fugg": "u"}
 
+websocket_helper_tasks = set()
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    # This function will be called whenever a new client connects to the server
     await websocket.accept()
+    # Start a task to write data to the websocket
+    write_task = asyncio.create_task(writer(websocket))
+    websocket_helper_tasks.add(write_task)
+    read_task = asyncio.create_task(reader(websocket))
+    websocket_helper_tasks.add(read_task)
+
     while True:
-        data = await websocket.receive()
-        sys.stdout.write("Received data: " + str(data) + "\n")
-        if data['type'] == "websocket.receive":
-            if is_json(data['text']):
-                #This is JSON
-                #Compare BACnet data to JSON
-                difference = get_diff({"keyA": "valA", "keyB": "valB"},{"keyA": "valC", "keyB": "valD", "Bullshitkey": "Bullshitvalue"})
-                sys.stdout.write(str(difference) + "\n")
+        try:
+            await asyncio.sleep(1)
+        except WebSocketDisconnect:
+            sys.stdout.write("Exception")
 
-                #Maybe can signal data is available. Make it a global variable and make it settable from the main.
 
-                pass
-            else:
-                #This isn't JSON
-                #Respond to commands
-                if data['text'] == "henlo":
-                    sys.stdout.write("henlo\n")
-                    pass
+async def writer(websocket):
+    while True:
+        try:
+            await websocket.send_text("Hello from the server!")
+            await asyncio.sleep(5)  # Wait for 5 second before sending the next message
 
-        if data['type'] == "websocket.disconnect":
-            sys.stdout.write("Disconnected...\n")
+        except (RuntimeError, asyncio.CancelledError) as error:
+            sys.stdout.write(str(error))
+            return
+        except WebSocketDisconnect:
+            sys.stdout.write("Exception Disconnect for writer")
+            return
+
+async def reader(websocket):
+    while True:
+        try:
+            data = await websocket.receive()
+            sys.stdout.write(str(data)+"\n")
+
+            if data['type'] == "websocket.disconnect":
+                for task in websocket_helper_tasks:                # Cancel read and write tasks when disconnecting
+                    task.cancel()
+                websocket_helper_tasks.clear()
+                sys.stdout.write("Disconnected...\n")
+                return
+
+        except (RuntimeError, asyncio.CancelledError) as error:
+            sys.stdout.write(str(error))
+            return
+        except WebSocketDisconnect:
+            sys.stdout.write("Exception Disconnect for reader")
             return
 
 # mounting flask into FastAPI
