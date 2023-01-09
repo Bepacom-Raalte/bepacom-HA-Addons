68b9243e8093855aec3bfa9418e4ba1417e67630
Luuk <lhwgeertsema@outlook.com>
2023-01-09 15:53:04 +0100

    Oke

--- bacnetinterface/rootfs/usr/bin/main.py
+++ bacnetinterface/rootfs/usr/bin/main.py
@@ -1,26 +1,31 @@
-#===================================================
+# ===================================================
 # Importing from libraries
-#=================================================== 
-from threading import Thread, Event
-from collections.abc import Callable
-from typing import Any
-from queue import Queue
-import uvicorn
-import webAPI as api
-import sys
+# ===================================================
 import pickle
-from bacpypes.debugging import bacpypes_debugging, ModuleLogger
-from bacpypes.consolelogging import ConfigArgumentParser, ConsoleLogHandler
-from bacpypes.core import run, deferred, stop, enable_sleeping
-from bacpypes.local.device import LocalDeviceObject
-from bacpypes.basetypes import PropertyReference, PropertyIdentifier, PropertyValue, RecipientProcess, Recipient, EventType, ServicesSupported
-from bacpypes.task import RecurringTask
-from BACnetIOHandler import BACnetIOHandler
-from bacpypes.pdu import GlobalBroadcast, RemoteBroadcast, LocalBroadcast, Address
+import sys
+from collections.abc import Callable
+from queue import Queue
+from threading import Event, Thread
+from typing import Any
 
-#===================================================
+import uvicorn
+from bacpypes.basetypes import (EventType, PropertyIdentifier,
+                                PropertyReference, PropertyValue, Recipient,
+                                RecipientProcess, ServicesSupported)
+from bacpypes.consolelogging import ConfigArgumentParser, ConsoleLogHandler
+from bacpypes.core import deferred, enable_sleeping, run, stop
+from bacpypes.debugging import ModuleLogger, bacpypes_debugging
+from bacpypes.local.device import LocalDeviceObject
+from bacpypes.pdu import (Address, GlobalBroadcast, LocalBroadcast,
+                          RemoteBroadcast)
+from bacpypes.task import RecurringTask
+
+import webAPI as api
+from BACnetIOHandler import BACnetIOHandler
+
+# ===================================================
 # Global variables
-#===================================================
+# ===================================================
 webserv: str = "127.0.0.1"
 port = 7813
 
@@ -31,16 +36,18 @@ rsvp = (True, None, None)
 _debug = 0
 _log = ModuleLogger(globals())
 
-#===================================================
+# ===================================================
 # Threads
-#=================================================== 
+# ===================================================
 # Uvicorn thread
 class uviThread(Thread):
     def run(self):
         uvicorn.run(api.app, host=webserv, port=port)
 
+
 class EventWatcherTask(RecurringTask):
     """Checks if event is true. When it is, do callback"""
+
     def __init__(self, event: Event(), callback: Callable, interval):
         RecurringTask.__init__(self, interval)
         self.event = event
@@ -48,13 +55,16 @@ class EventWatcherTask(RecurringTask):
 
         # install it
         self.install_task()
+
     def process_task(self):
         if self.event.is_set():
             self.callback()
             self.event.clear()
 
+
 class QueueWatcherTask(RecurringTask):
     """Checks if queue has items. When it has, do callback"""
+
     def __init__(self, queue: Queue(), callback: Callable, interval):
         RecurringTask.__init__(self, interval)
         self.queue = queue
@@ -62,61 +72,66 @@ class QueueWatcherTask(RecurringTask):
 
         # install it
         self.install_task()
+
     def process_task(self):
         if self.queue.empty():
             return
         queue_item = self.queue.get()
         self.callback(queue_item)
 
+
 class RefreshDict(RecurringTask):
     """Checks if queue has items. When it has, do callback"""
+
     def __init__(self, interval):
         RecurringTask.__init__(self, interval)
 
         # install it
         self.install_task()
+
     def process_task(self):
-        for device, devicedata in this_application.BACnetDeviceDict.items():
-            objectlist = []
-            for object, objectdata in devicedata.items():
-                if isinstance(objectdata, dict):
-                    objectlist.append(object)
-            this_application.ReadPropertyMultiple(
-                objectList=objectlist,
-                propertyList=this_application.propertyList,
-                address=this_application.dev_id_to_addr(device)
-                )
+        this_application.read_entire_dict()
+
 
 def write_from_dict(dict_to_write: dict):
     deviceID = get_key(dict_to_write)
     for object in dict_to_write[deviceID]:
         for property in dict_to_write[deviceID][object]:
             prop_value = dict_to_write[deviceID][object].get(property)
-            this_application.WriteProperty(object, property, prop_value, this_application.dev_id_to_addr(deviceID))
+            this_application.WriteProperty(
+                object, property, prop_value, this_application.dev_id_to_addr(deviceID)
+            )
+
 
 def get_key(dictionary: dict) -> str:
+    """Return the first key"""
     for key, value in dictionary.items():
         return key
 
-        
-#===================================================
+
+def read_all_from_dict():
+    """Read all objects from every device included in the dictionary"""
+    this_application.read_entire_dict()
+
+
+# ===================================================
 # Main
-#=================================================== 
+# ===================================================
 def main():
-    #===================================================
+    # ===================================================
     # parse bacpypes.ini
-    #===================================================
+    # ===================================================
     args = ConfigArgumentParser(description=__doc__).parse_args()
-    
-    #===================================================
+
+    # ===================================================
     # Uvicorn server
-    #===================================================
+    # ===================================================
     server = uviThread()
     server.start()
 
-    #===================================================
+    # ===================================================
     # BACnet server
-    #===================================================
+    # ===================================================
     global this_application
     global this_device
 
@@ -127,11 +142,11 @@ def main():
         maxApduLengthAccepted=int(args.ini.maxapdulengthaccepted),
         segmentationSupported=args.ini.segmentationsupported,
         vendorIdentifier=int(args.ini.vendoridentifier),
-        description="BACnet Add-on for Home Assistant"
-        )
+        description="BACnet Add-on for Home Assistant",
+    )
 
     # provide max segments accepted if any kind of segmentation supported
-    if args.ini.segmentationsupported != 'noSegmentation':
+    if args.ini.segmentationsupported != "noSegmentation":
         this_device.maxSegmentsAccepted = int(args.ini.maxsegmentsaccepted)
 
     # make a simple application
@@ -141,15 +156,17 @@ def main():
     # Coupling of FastAPI and BACnetIOHandler
     api.BACnetDeviceDict = this_application.BACnetDeviceDict
     api.threadingUpdateEvent = this_application.updateEvent
-    who_is_watcher = EventWatcherTask(api.threadingWhoIsEvent, this_application.who_is, 2000)
-    i_am_watcher = EventWatcherTask(api.threadingIAmEvent,this_application.i_am, 2000)
+    who_is_watcher = EventWatcherTask(
+        api.threadingWhoIsEvent, this_application.who_is, 2000
+    )
+    i_am_watcher = EventWatcherTask(api.threadingIAmEvent, this_application.i_am, 2000)
+    read_watcher = EventWatcherTask(api.threadingReadAllEvent, read_all_from_dict, 2000)
     write_queue_watcher = QueueWatcherTask(api.writeQueue, write_from_dict, 1000)
     dict_refresher = RefreshDict(60000)
-    
+
     while True:
         run()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
-
-
--- bacnetinterface/rootfs/usr/bin/webAPI.py
+++ bacnetinterface/rootfs/usr/bin/webAPI.py
@@ -1,57 +1,56 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.middleware.wsgi import WSGIMiddleware
-from flask import Flask, render_template
-from typing import Union, Any
-from queue import Queue
-import threading
 import asyncio
 import json
 import sys
+import threading
+from queue import Queue
+from typing import Any, Union
 
-#===================================================
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.wsgi import WSGIMiddleware
+from flask import Flask, render_template
+
+# ===================================================
 # Global variables
-#===================================================
+# ===================================================
 
 BACnetDeviceDict = dict()
 websocket_helper_tasks = set()
 threadingUpdateEvent: threading.Event = threading.Event()
 threadingWhoIsEvent: threading.Event = threading.Event()
 threadingIAmEvent: threading.Event = threading.Event()
+threadingReadAllEvent: threading.Event = threading.Event()
 writeQueue: Queue = Queue()
 
-#===================================================
-# Helperfunctions
-#===================================================
 
 def BACnetToDict(BACnetDict):
-    """Convert the BACnet dict to something that can be converted to JSON"""
-    #### DeviceID: ObjectID : Property : Waarde
+    """Convert the BACnet dict to something that can be converted to JSON."""
     propertyFilter = (
-        'objectIdentifier', 
-        'objectName', 
-        'objectType',
-        'description',
-        'presentValue',
-        'outOfService',
-        'eventState',
-        'reliability',
-        'statusFlags',
-        'units',
-        'covIncrement',
-        'vendorName',
-        'modelName',
-        'notificationClass'
-        )
+        "objectIdentifier",
+        "objectName",
+        "objectType",
+        "description",
+        "presentValue",
+        "outOfService",
+        "eventState",
+        "reliability",
+        "statusFlags",
+        "units",
+        "covIncrement",
+        "vendorName",
+        "modelName",
+        "stateText",
+        "numberOfStates",
+    )
     devicesDict = {}
     for deviceID in BACnetDict.keys():
         deviceDict = {}
-        deviceIDstr = ':'.join(map(str,deviceID))
+        deviceIDstr = ":".join(map(str, deviceID))
         for objectID in BACnetDict[deviceID].keys():
             objectDict = {}
-            if objectID in ('address', 'deviceIdentifier'):
+            if objectID in ("address", "deviceIdentifier"):
                 continue
-            objectIDstr = ':'.join(map(str,objectID))
-            for propertyID,value in BACnetDict[deviceID][objectID].items():
+            objectIDstr = ":".join(map(str, objectID))
+            for propertyID, value in BACnetDict[deviceID][objectID].items():
                 if propertyID in propertyFilter:
                     if isinstance(value, (int, float, bool, str)):
                         objectDict.update({propertyID: value})
@@ -60,10 +59,11 @@ def BACnetToDict(BACnetDict):
 
             deviceDict.update({objectIDstr: objectDict})
         devicesDict.update({deviceIDstr: deviceDict})
-    return devicesDict         
+    return devicesDict
+
 
 def DictToBACnet(dictionary: dict) -> dict:
-    """Create a new dictionary with the converted keys"""
+    """Create a new dictionary with the converted keys."""
     converted_dict = {str_to_tuple(k): v for k, v in dictionary.items()}
     # Recursively convert the keys in any inner dictionaries
     for k, v in converted_dict.items():
@@ -73,52 +73,64 @@ def DictToBACnet(dictionary: dict) -> dict:
                 converted_dict[k] = DictToBACnet(v)
     return converted_dict
 
+
 def str_to_tuple(input_str: str) -> tuple:
-    """Convert ObjectIdentifier string to tuple"""
+    """Convert ObjectIdentifier string to tuple."""
     split_str = input_str.split(":")
     return (split_str[0], int(split_str[1]))
 
+
 async def on_start():
-    # Startup delay so BACnetIOHandler can subscribe safely to everything
+    """Startup sequence of FastAPI."""
     await asyncio.sleep(4)
-    
-#===================================================
-# Flask setup (WebUI)
-#===================================================
-flask_app = Flask("WebUI", template_folder='/usr/bin/templates')
+
+
+flask_app = Flask("WebUI", template_folder="/usr/bin/templates")
+
 
 @flask_app.route("/")
 def flask_main():
+    """WebUI page for Home Assistant."""
     return render_template("index.html")
 
-#===================================================
-# FastAPI setup
-#===================================================
+
 app = FastAPI(on_startup=[on_start])
+
 
 @app.get("/apiv1/json")
 async def get_entire_dict():
-    """Return all devices and their values"""
+    """Return all devices and their values."""
     global BACnetDeviceDict
     return BACnetToDict(BACnetDeviceDict)
 
+
 @app.get("/apiv1/command/whois")
 async def whois_command():
-    """Send a Who Is Request over the BACnet network"""
+    """Send a Who Is Request over the BACnet network."""
     threadingWhoIsEvent.set()
     return
 
+
 @app.get("/apiv1/command/iam")
 async def iam_command():
-    """Send an I Am Request over the BACnet network"""
+    """Send an I Am Request over the BACnet network."""
     threadingIAmEvent.set()
     return
 
+
+@app.get("/apiv1/command/readall")
+async def read_all():
+    """Send a Read Request to all devices on the BACnet network."""
+    threadingReadAllEvent.set()
+    return
+
+
 # Any commands or not variable paths should go above here... FastAPI will use it as a variable if you make a new path below this.
+
 
 @app.get("/apiv1/{deviceid}")
 async def read_deviceid_dict(deviceid: str):
-    """Read a device"""
+    """Read a device."""
     global BACnetDeviceDict
     var = BACnetToDict(BACnetDeviceDict)
     try:
@@ -126,9 +138,10 @@ async def read_deviceid_dict(deviceid: str):
     except Exception as e:
         return "Error: " + str(e)
 
+
 @app.get("/apiv1/{deviceid}/{objectid}")
 async def read_objectid_dict(deviceid: str, objectid: str):
-    """Read an object from a device"""
+    """Read an object from a device."""
     try:
         global BACnetDeviceDict
         var = BACnetToDict(BACnetDeviceDict)
@@ -139,15 +152,17 @@ async def read_objectid_dict(deviceid: str, objectid: str):
     except Exception as e:
         return "Error: " + str(e)
 
+
 @app.get("/apiv1/{deviceid}/{objectid}/{propertyid}")
 async def read_objectid_property(deviceid: str, objectid: str, propertyid: str):
-    """Read a property of an object from a device"""
+    """Read a property of an object from a device."""
     global BACnetDeviceDict
     var = BACnetToDict(BACnetDeviceDict)
     try:
         return var[deviceid][objectid][propertyid]
     except Exception as e:
         return "Error: " + str(e)
+
 
 @app.post("/apiv1/{deviceid}/{objectid}")
 async def write_objectid_property(
@@ -157,39 +172,39 @@ async def write_objectid_property(
     objectName: Union[str, None] = None,
     objectType: Union[str, None] = None,
     description: Union[str, None] = None,
-    presentValue: Union[int,float, str, None] = None,
+    presentValue: Union[int, float, str, None] = None,
     outOfService: Union[bool, None] = None,
     eventState: Union[str, None] = None,
     reliability: Union[str, None] = None,
     statusFlags: Union[str, None] = None,
     units: Union[str, None] = None,
     covIncrement: Union[int, float, None] = None,
-    ):
-    """Write to a property of an object from a device"""
-    property_dict:dict[dict,Any] = {}
-    dict_to_write:dict[dict, Any] = {}
+):
+    """Write to a property of an object from a device."""
+    property_dict: dict[dict, Any] = {}
+    dict_to_write: dict[dict, Any] = {}
     if objectIdentifier != None:
-        property_dict.update({'objectIdentifier': objectIdentifier})
+        property_dict.update({"objectIdentifier": objectIdentifier})
     if objectName != None:
-        property_dict.update({'objectName': objectName})
+        property_dict.update({"objectName": objectName})
     if objectType != None:
-        property_dict.update({'objectType': objectType})
+        property_dict.update({"objectType": objectType})
     if description != None:
-        property_dict.update({'description': description})
+        property_dict.update({"description": description})
     if presentValue != None:
-        property_dict.update({'presentValue': presentValue})
+        property_dict.update({"presentValue": presentValue})
     if outOfService != None:
-        property_dict.update({'outOfService': outOfService})
+        property_dict.update({"outOfService": outOfService})
     if eventState != None:
-        property_dict.update({'eventState': eventState})
+        property_dict.update({"eventState": eventState})
     if reliability != None:
-        property_dict.update({'reliability': reliability})
+        property_dict.update({"reliability": reliability})
     if statusFlags != None:
-        property_dict.update({'statusFlags': statusFlags})
+        property_dict.update({"statusFlags": statusFlags})
     if units != None:
-        property_dict.update({'units': units})
+        property_dict.update({"units": units})
     if covIncrement != None:
-        property_dict.update({'covIncrement': covIncrement})
+        property_dict.update({"covIncrement": covIncrement})
 
     if property_dict == {}:
         return "No property values"
@@ -206,9 +221,10 @@ async def write_objectid_property(
     writeQueue.put(bacnet_dict)
     return "Successfully put in Write Queue"
 
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    """This function will be called whenever a new client connects to the server"""
+    """This function will be called whenever a new client connects to the server."""
     updateEvent = asyncio.Event()
     await websocket.accept()
     # Start a task to write data to the websocket
@@ -226,6 +242,7 @@ async def websocket_endpoint(websocket: WebSocket):
 
 
 async def websocket_writer(websocket: WebSocket, updateEvent: asyncio.Event):
+    """Writer task for when a websocket is opened"""
     global BACnetDeviceDict
     while True:
         try:
@@ -239,19 +256,25 @@ async def websocket_writer(websocket: WebSocket, updateEvent: asyncio.Event):
             sys.stdout.write("Exception Disconnect for writer")
             return
 
+
 async def websocket_reader(websocket: WebSocket, updateEvent: asyncio.Event):
+    """Reader task for when a websocket is openened."""
     while True:
         try:
             data = await websocket.receive()
-            #sys.stdout.write(str(data)+"\n")
-            if data['type'] == "websocket.disconnect":
-                for task in websocket_helper_tasks:                # Cancel read and write tasks when disconnecting
+            # sys.stdout.write(str(data)+"\n")
+            if data["type"] == "websocket.disconnect":
+                for (
+                    task
+                ) in (
+                    websocket_helper_tasks
+                ):  # Cancel read and write tasks when disconnecting
                     task.cancel()
                 websocket_helper_tasks.clear()
                 sys.stdout.write("Disconnected...\n")
                 return
-            if "{" in data['text']:
-                message = data['text']
+            if "{" in data["text"]:
+                message = data["text"]
                 message = json.loads(message)
                 bacnet_dict = DictToBACnet(message)
                 global writeQueue
@@ -264,7 +287,9 @@ async def websocket_reader(websocket: WebSocket, updateEvent: asyncio.Event):
             sys.stdout.write("Exception Disconnect for reader")
             return
 
+
 async def on_value_changed(updateEvent: asyncio.Event):
+    """Watch whether value has changed."""
     global BACnetDeviceDict
     try:
         while True:
@@ -273,9 +298,10 @@ async def on_value_changed(updateEvent: asyncio.Event):
             else:
                 threadingUpdateEvent.clear()
                 updateEvent.set()
-                #sys.stdout.write("Update set...\n")
+                # sys.stdout.write("Update set...\n")
     except:
         sys.stdout.write("Exited on_change\n")
-        
+
+
 # mounting flask into FastAPI
 app.mount("/webapp", WSGIMiddleware(flask_app))
