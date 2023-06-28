90773abf14268cb23966aa5278a0be63204631d9
LGeertsema <LGeertsema@SWEWP0011>
2023-06-28 08:11:20 +0200

    Revert change

--- bacnetinterface/rootfs/usr/bin/BACnetIOHandler.py
+++ bacnetinterface/rootfs/usr/bin/BACnetIOHandler.py
@@ -387,7 +387,7 @@ class BACnetIOHandler(NormalApplication):
                     device_identifier=device_identifier,
                     object_identifier=ObjectIdentifier(object_id),
                     confirmed_notifications=True,
-                    lifetime=0,
+                    lifetime=28799,
                 )
 
     async def create_subscription_task(
